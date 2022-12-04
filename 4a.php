915095f0eb5e600fe881ba8a0e60c3332f01ae1a
maarten <maarten@peppered.com>
2022-12-04 17:43:58 +0100

    day 4

---
+++ 4a.php
@@ -0,0 +1,13 @@
+w<?php
+
+$input = file('input4.txt');
+$total = 0;
+foreach ($input as $line) {
+    $sections = explode(',', str_replace("\n", '', trim($line)));
+    [$min1, $max1] = explode('-', $sections[0]);
+    [$min2, $max2] = explode('-', $sections[1]);
+    if (($min1 <= $min2 && $max1 >= $max2) || ($min2 <= $min1 && $max2 >= $max1)) {
+        $total++;
+    }
+}
+echo $total;
---
+++ 4b.php
@@ -0,0 +1,13 @@
+<?php
+
+$input = file('input4.txt');
+$total = 0;
+foreach ($input as $line) {
+    $sections = explode(',', str_replace("\n", '', trim($line)));
+    [$min1, $max1] = explode('-', $sections[0]);
+    [$min2, $max2] = explode('-', $sections[1]);
+    if (($min2 >= $min1 && $min2 <= $max1) || ($min1 >= $min2 && $min1 <= $max2)) {
+        $total++;
+    }
+}
+echo $total;
