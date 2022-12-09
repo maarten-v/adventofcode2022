89232533c7bb536be32d2e96175ecdf35284937c
Maarten <maarten@peppered.com>
2022-12-09 23:44:30 +0100

    5a

--- 5a.php
+++ 5a.php
@@ -1,7 +1,6 @@
 <?php
-
 $input = file('input5.txt');
-$crates = [];
+$stacks = [];
 $cratesAreParsed = false;
 $lineCounter = 0;
 foreach ($input as $line) {
@@ -10,14 +9,30 @@ foreach ($input as $line) {
 
     for ($i = 1; $i < $length && !$cratesAreParsed; $i += 4) {
         $crate = $line[$i];
-        $crateArray[$i - 1]
+        $stack = ($i - 1) / 4;
         if ($crate === '1') {
-//            var_dump()
             $cratesAreParsed = true;
-            break;
+            break 2;
         }
-        echo $crate . '-';
+        if (!isset($stacks[$stack])) {
+            $stacks[$stack] = [];
+        }
+        if ($crate !== ' ') {
+            array_unshift($stacks[$stack], $crate);
+        }
     }
     $lineCounter++;
-
 }
+$lineAmount = count($input);
+for ($i = $lineCounter + 2; $i < $lineAmount; $i++) {
+    preg_match_all("/\d+/", $input[$i], $matches);
+    [$amount, $from, $to] = $matches[0];
+    for ($j = 0; $j < $amount; $j++) {
+        $stacks[$to - 1][] = array_pop($stacks[$from - 1]);
+    }
+}
+$result = '';
+foreach ($stacks as $stack) {
+    $result .= end($stack);
+}
+echo $result;
