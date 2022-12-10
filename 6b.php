20222d13ecfb37b8959791e2e40a1717304bc058
maarten <maarten@peppered.com>
2022-12-10 18:58:58 +0100

    6b

---
+++ 6b.php
@@ -0,0 +1,11 @@
+<?php
+$input = file('input6.txt')[0];
+$length = strlen($input);
+for ($i = 0; $i < ($length - 14); $i++) {
+    $marker = substr($input, $i, 14);
+    $markerAsArray = str_split($marker);
+    if ($markerAsArray === array_unique($markerAsArray)) {
+        echo ($i + 14);
+        break;
+    }
+}
