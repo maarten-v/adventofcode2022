d2fa132e90cd0a9752888704cf50ec0498c141a1
Maarten <maarten@peppered.com>
2022-12-13 22:53:55 +0100

    wip 7a

--- 7a.php
+++ 7a.php
@@ -1,18 +1,40 @@
 <?php
 $input = file('input7.txt');
 $structure = [];
-$dir = $structure;
 /** @var string $line */
-foreach ($input as $line) {
-    if ($line === '$ ls') {
-        continue;
+foreach ($input as $lineNumber => $line) {
+    $line = str_replace("\n", '', $line);
+    if ($line === '$ cd /') {
+        readFolder($lineNumber + 1, '/');
     }
-    if (str_starts_with('$ cd', $line)) {
-        $dir['sr'] = [];
-        $dir = $dir['sr'];
-    }
-    if (is_numeric($line[0])) {
-        preg_match("/\d+/", $line, $size);
-        $dir[] = $size;
+
+}
+
+function readFolder($startLine, $folder) {
+    global $structure, $input;
+    $tweedekeer = false;
+    for ($i = $startLine; $i < count($input); $i++) {
+        if ($i === 1) {
+            if ($tweedekeer) {
+                break;
+            }
+            $tweedekeer = true;
+        }
+        echo $i.'-';
+        $line = str_replace("\n", '', $input[$i]);
+        if ($line === '$ cd ..') {
+            echo 'x' . $i . 'x';
+            return $i;
+        }
+        if (str_starts_with($line, '$ cd ')) {
+            echo 'zit in cd';
+            $i = readFolder($i + 1, $folder[substr($line, 5)]);
+        }
+        if (is_numeric($line[0])) {
+            preg_match("/\d+/", $line, $size);
+            $structure[$folder][] = $size;
+        }
     }
 }
+
+var_dump($structure);
