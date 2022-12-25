260e277e1853dd8d56ff8ea9c4d5d53a1c89a368
maarten <maarten@peppered.com>
2022-12-25 21:56:23 +0100

    8a

--- 8a.php
+++ 8a.php
@@ -8,16 +8,13 @@ $colCount = strlen($input[0]);
 for ($i = 0; $i < $rowCount; $i++) {
     $trees[$i] = str_split(str_replace("\n", '', $input[$i]));
 }
-$heights = [];
+$visibleTrees = [];
 
 // left to right
 for ($row = 0; $row <$rowCount; $row++) {
     $previousMaxHeight = -1;
     for ($col = 0; $col <$rowCount; $col++) {
-        if ($trees[$row][$col] > $previousMaxHeight) {
-            $previousMaxHeight = $trees[$row][$col];
-            $heights[$row][$col] = 1;
-        }
+        $previousMaxHeight = setVisible($previousMaxHeight, $row, $col);
     }
 }
 
@@ -25,10 +22,7 @@ for ($row = 0; $row <$rowCount; $row++) {
 for ($row = 0; $row <$rowCount; $row++) {
     $previousMaxHeight = -1;
     for ($col = $rowCount - 1; $col >= 0; $col--) {
-        if ($trees[$row][$col] > $previousMaxHeight) {
-            $previousMaxHeight = $trees[$row][$col];
-            $heights[$row][$col] = 1;
-        }
+        $previousMaxHeight = setVisible($previousMaxHeight, $row, $col);
     }
 }
 
@@ -36,10 +30,7 @@ for ($row = 0; $row <$rowCount; $row++) {
 for ($col = 0; $col < $colCount - 1; $col++) {
     $previousMaxHeight = -1;
     for ($row = 0; $row < $rowCount; $row++) {
-        if ($trees[$col][$row] > $previousMaxHeight) {
-            $previousMaxHeight = $trees[$col][$row];
-            $heights[$row][$col] = 1;
-        }
+        $previousMaxHeight = setVisible($previousMaxHeight, $row, $col);
     }
 }
 
@@ -47,15 +38,21 @@ for ($col = 0; $col < $colCount - 1; $col++) {
 for ($col = 0; $col < $colCount - 1; $col++) {
     $previousMaxHeight = -1;
     for ($row = $rowCount - 1; $row >= 0; $row--) {
-        if ($trees[$col][$row] > $previousMaxHeight) {
-            $previousMaxHeight = $trees[$col][$row];
-            $heights[$row][$col] = 1;
-        }
+        $previousMaxHeight = setVisible($previousMaxHeight, $row, $col);
     }
 }
 
+function setVisible(int $previousMaxHeight, int $row, int $col): int {
+    global $trees, $visibleTrees;
+    if ($trees[$row][$col] > $previousMaxHeight) {
+        $previousMaxHeight = $trees[$row][$col];
+        $visibleTrees[$row][$col] = 1;
+    }
+    return $previousMaxHeight;
+}
+
 $sum = 0;
-foreach ($heights as $row) {
+foreach ($visibleTrees as $row) {
     $sum += array_sum($row);
 }
 echo $sum;
