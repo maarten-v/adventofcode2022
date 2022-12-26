b4e064dc01533a1d8ca16624ba2a2b9133a407a1
maarten <maarten@peppered.com>
2022-12-26 14:43:28 +0100

    8b

--- 8b.php
+++ 8b.php
@@ -8,64 +8,54 @@ $colCount = strlen($input[0]) - 1;
 for ($i = 0; $i < $rowCount; $i++) {
     $trees[$i] = str_split(str_replace("\n", '', $input[$i]));
 }
-$visibleTrees = [];
+$allScores = [];
 
 for ($globalrow = 0; $globalrow < $rowCount; $globalrow++) {
     for ($globalcol = 0; $globalcol < $colCount; $globalcol++) {
         $height = $trees[$globalrow][$globalcol];
+
         // left to right
         $visibleTreesCounter = 0;
-//        echo "colcount $colCount \n\r";
         for ($col = $globalcol; $col < $colCount - 1; $col++) {
-//            echo "col $col\n\r";
-//            echo 'y'.$trees[$globalrow][$col + 1]. '-'.$height . 'x' . "\r\n";
+            $visibleTreesCounter++;
             if ($trees[$globalrow][$col + 1] >= $height) {
                 break;
             }
-            $visibleTreesCounter++;
         }
-        echo $visibleTreesCounter;
+        $leftToRight = $visibleTreesCounter;
+
+        // right to left
+        $visibleTreesCounter = 0;
+        for ($col = $globalcol; $col > 0; $col--) {
+            $visibleTreesCounter++;
+            if ($trees[$globalrow][$col -1] >= $height) {
+                break;
+            }
+        }
+        $rightToLeft = $visibleTreesCounter;
+
+        // top to bottom
+        $visibleTreesCounter = 0;
+        for ($row = $globalrow; $row < $rowCount - 1; $row++) {
+            $visibleTreesCounter++;
+            if ($trees[$row + 1][$globalcol] >= $height) {
+                break;
+            }
+        }
+        $topToBottom = $visibleTreesCounter;
+
+        // bottom to top
+        $visibleTreesCounter = 0;
+        for ($row = $globalrow; $row > 0; $row--) {
+            $visibleTreesCounter++;
+            if ($trees[$row - 1][$globalcol] >= $height) {
+                break;
+            }
+        }
+        $bottomToTop = $visibleTreesCounter;
+
+        $allScores[] = $leftToRight * $rightToLeft * $topToBottom * $bottomToTop;
     }
-    echo "\n\r";
 }
 
-exit();
-
-// right to left
-for ($row = 0; $row <$rowCount; $row++) {
-    $previousMaxHeight = -1;
-    for ($col = $rowCount - 1; $col >= 0; $col--) {
-        $previousMaxHeight = setVisible($previousMaxHeight, $row, $col);
-    }
-}
-
-// top to bottom
-for ($col = 0; $col < $colCount - 1; $col++) {
-    $previousMaxHeight = -1;
-    for ($row = 0; $row < $rowCount; $row++) {
-        $previousMaxHeight = setVisible($previousMaxHeight, $row, $col);
-    }
-}
-
-//bottom to top
-for ($col = 0; $col < $colCount - 1; $col++) {
-    $previousMaxHeight = -1;
-    for ($row = $rowCount - 1; $row >= 0; $row--) {
-        $previousMaxHeight = setVisible($previousMaxHeight, $row, $col);
-    }
-}
-
-function setVisible(int $previousMaxHeight, int $row, int $col): int {
-    global $trees, $visibleTrees;
-    if ($trees[$row][$col] > $previousMaxHeight) {
-        $previousMaxHeight = $trees[$row][$col];
-        $visibleTrees[$row][$col] = 1;
-    }
-    return $previousMaxHeight;
-}
-
-$sum = 0;
-foreach ($visibleTrees as $row) {
-    $sum += array_sum($row);
-}
-echo $sum;
+echo max($allScores);
